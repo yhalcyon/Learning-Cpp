713c51b6f0dd4d949c535301bf2930a5b84b3b8d
yhalcyon <linglin1989@gmail.com>
2015-08-29 12:22:47 -0600

    Class and Objects
    Class is a crucial important concept in C++ and it is the foundation for
    the object-oriented programming language. In this module, we will learn
    the constructor and deconstructor.

---
+++ Class and Objects/part2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+class Sample{
+public:
+	int v;
+	Sample(int n):v(n){ }
+	Sample(Sample &a){
+		v = 2*a.v;
+	}
+};
+
+int main(){
+	Sample a(5);
+	Sample b = a;
+	cout<<b.v<<endl;
+	return 0;
+}
